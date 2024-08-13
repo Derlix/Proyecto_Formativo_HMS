8652abdbe57ab50aea7230c37746d43592023464
nicolas <tapasconicolas13@gmail.com>
2024-08-12 23:00:06 -0500

    Modo oscuro check-out

--- 2_frontend_Vue.js/src/views/nico-views/CheckOut.vue
+++ 2_frontend_Vue.js/src/views/nico-views/CheckOut.vue
@@ -10,80 +10,80 @@ import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
             <h1 class="bg-blue-400 p-5 text-white font-semibold rounded-t-lg border-2 border-black border-b-0">Check-Out</h1>
             <div class="border-2 border-black pb-4 pl-4 dark:text-white">
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Nombre:</h2>
+                    <h2 class="text-start dark:text-white">Nombre:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="Juan">
-                    <h2 class="text-start">Seguro:</h2>
+                    <h2 class="text-start dark:text-white">Seguro:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="$25.000">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">llegada:</h2>
+                    <h2 class="text-start dark:text-white">llegada:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="date">
-                    <h2 class="text-start">Anticipo:</h2>
+                    <h2 class="text-start dark:text-white">Anticipo:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="$5.000">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Salida:</h2>
+                    <h2 class="text-start dark:text-white">Salida:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="date">
-                    <h2 class="text-start">Miscelaneos:</h2>
+                    <h2 class="text-start dark:text-white">Miscelaneos:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="12%">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Registro N°:</h2>
+                    <h2 class="text-start dark:text-white">Registro N°:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="4233">
-                    <h2 class="text-start">Transferencia Debito:</h2>
+                    <h2 class="text-start dark:text-white">Transferencia Debito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="Si">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Habitacion N°:</h2>
+                    <h2 class="text-start dark:text-white">Habitacion N°:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="104">
-                    <h2 class="text-start">Total Debitos:</h2>
+                    <h2 class="text-start dark:text-white">Total Debitos:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="3">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Precio por noche:</h2>
+                    <h2 class="text-start dark:text-white">Precio por noche:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="$40.000">
-                    <h2 class="text-start">Particular:</h2>
+                    <h2 class="text-start dark:text-white">Particular:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="No">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Noches de estadia:</h2>
+                    <h2 class="text-start dark:text-white">Noches de estadia:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="5">
-                    <h2 class="text-start">Transferencia Credito:</h2>
+                    <h2 class="text-start dark:text-white">Transferencia Credito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="No">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Impuestos:</h2>
+                    <h2 class="text-start dark:text-white">Impuestos:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="12%">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Tipo de habitacion:</h2>
+                    <h2 class="text-start dark:text-white">Tipo de habitacion:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="VIP">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Acompañantes:</h2>
+                    <h2 class="text-start dark:text-white">Acompañantes:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="5">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Deposito:</h2>
+                    <h2 class="text-start dark:text-white">Deposito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="number" placeholder="50.000">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Tarjeta Credito:</h2>
+                    <h2 class="text-start dark:text-white">Tarjeta Credito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="No">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Total Credito:</h2>
+                    <h2 class="text-start dark:text-white">Total Credito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="No">
                 </div>
                 <div class="grid grid-cols-4 font-medium p-2 gap-3 text-black">
-                    <h2 class="text-start">Total Credito:</h2>
+                    <h2 class="text-start dark:text-white">Total Credito:</h2>
                     <input class="border-none bg-gray-300 rounded-2xl text-center h-7 w-48" type="text" placeholder="No">
                 </div>
             </div>
             <div class="grid grid-cols-2 p-5 text-black font-bold">
-                <h2>TOTAL A PAGAR:</h2>
-                <span class="text-end">12.900.000</span>
-                <h2 class="mt-5">Metodos de pago:</h2>
+                <h2 class="dark:text-white">TOTAL A PAGAR:</h2>
+                <span class="text-end dark:text-white">12.900.000</span>
+                <h2 class="mt-5 dark:text-white">Metodos de pago:</h2>
             </div>
             <div class="grid grid-cols-3 font-medium gap-48 text-center px-10">
                 <button class="bg-blue-950 text-white rounded-xl h-10">Efectivo</button>
