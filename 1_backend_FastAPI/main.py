adfb0dd49306938266d436b8d86f799487fb050e
“Cristian <cris.arce1864@gmail.com>
2024-08-30 14:56:10 -0500

    endpoints caracteristicas

--- 1_backend_FastAPI/main.py
+++ 1_backend_FastAPI/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from appv1.routers.arce_routers import CaracteristicaRouter
 from appv1.routers.nicolas_route import impuesto
 
 app = FastAPI()
 
 app.include_router(impuesto.router, prefix="/impuesto", tags=["impuesto"])
+app.include_router(CaracteristicaRouter.router, prefix="/caracteristica", tags=["caracteristica"])
 
 @app.get("/")
 def read_root():
