cda9de3a3531794e5f83992dfa81a3a8adb79932
Sofia jaramillo <jaramillosofia871@gmail.com>
2024-08-30 16:07:10 -0500

    Crud Check In

--- 1_backend_FastAPI/models/models_miguel/check_in.py
+++ 1_backend_FastAPI/models/models_miguel/check_in.py
@@ -1,18 +1,29 @@
+import enum
 from sqlalchemy import VARCHAR, create_engine, Column, Integer, Enum, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+class EquipajeEnum(enum.Enum):
+    si = "SI"
+    no = "NO"
+
+class LlegadaEnum(enum.Enum):
+    con_reserva = "CON RESERVA"
+    sin_reserva = "SIN RESERVA"
+
+class MedioLlegadaEnum(enum.Enum):
+    aereo = "AEREO"
+    terrestre = "TERRESTRE"
+
 class CheckIn(Base):
     __tablename__ = 'check_in'
     
-    id_check_in = Column(Integer, primary_key=True, unique=True)
+    id_check_in = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     id_reserva = Column(Integer, ForeignKey('reservas.id_reserva'))
-    medio_llegada = Column(Enum('AEREO', 'TERRESTRE'))
-    llegada_situacion = Column(Enum('CON RESERVA', 'SIN RESERVA'))
-    equipaje = Column(Enum('SI', 'NO'))
-    suma_en_letras = Column(VARCHAR(40))
-
-
-
+    medio_llegada = Column((MedioLlegadaEnum))
+    llegada_situacion = Column((LlegadaEnum))
+    equipaje = Column((EquipajeEnum))
+    
+    reserva = relationship("Reserva")
