e0056bf64d1463eb44886471b5337adb1fc64a13
FELIPE CARDONA <felipitocard0521@gmail.com>
2024-09-11 22:22:38 -0500

    cambios vista

--- 2_frontend_Vue.js/src/services/hotelService.js
+++ 2_frontend_Vue.js/src/services/hotelService.js
@@ -1,31 +1,29 @@
 import api from './api';
 
-
-const createHotel = async (nombre,ubicacion,direccion,telefono) => {
-      try {
-        // Realizar una solicitud POST a la API para crear un nuevo hotel
-       
-        const response = await api.post('/hoteles/hoteles/create/', {
-            nombre: nombre,
-            ubicacion: ubicacion,
-            direccion: direccion,
-            telefono: telefono
-          }
-        
-        );
-        
-        // Manejo de la respuesta (puedes ajustar según tus necesidades)
-        console.log('Hotel creado con éxito:', response.data);
-    
-        // Retorna los datos del hotel creado o cualquier respuesta necesaria
-        return response;
-      } catch (error) {
-        // Manejo de errores
-        console.error('Error al crear el hotel:', error.response ? error.response.data : error.message);
-        throw error; // Lanza el error para que el llamador lo maneje
-      }
+export const createHotel = async (nombre, ubicacion, direccion, telefono) => {
+  try {
+    const payload = {
+      nombre,
+      ubicacion,
+      direccion,
+      telefono,
     };
 
+    const response = await api.post('/hoteles/hoteles/create/', payload, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      throw error.response; // Devuelve el error original de la API
+    } else {
+      throw new Error('Error de red o de servidor');
+    }
+  }
+};
     export const getHotels = async () => {
       try {
         const response = await api.get('/hoteles/hoteles/get_all/', {
@@ -44,45 +42,51 @@ const createHotel = async (nombre,ubicacion,direccion,telefono) => {
         }
       }
     };
-
-    export const updateUser = async (nombre,ubicacion,direccion,telefono) => {
+    export const getHotelsByPage = async (page = 1, pageSize = 10) => {
       try {
-        const response = await api.put(`/users/update/?user_id=${userId}`, {
+        const response = await api.get(`/hoteles/hoteles-by-page/?page=${page}&page_size=${pageSize}`);
+        return response;
+      } catch (error) {
+        if (error.response) {
+          throw error.response; // Devuelve el error original de la API
+        } else {
+          throw new Error('Error de red o de servidor');
+        }
+      }
+    };
+
+    export const updateHotel = async (hotel_id,nombre,ubicacion,direccion,telefono) => {
+      try {
+        const response = await api.put(`/hoteles/hoteles/update/?hotel_id=${hotel_id}`, {
           nombre: nombre,
           ubicacion: ubicacion,
           direccion: direccion,
           telefono: telefono
-        }, {
-          headers: {
-            'Content-Type': 'application/json',
-           
-          }
         });
         return response;
       } catch (error) {
         if (error.response) {
-          throw error; // Lanza el error para que lo maneje el store
+          throw error.response; // Devuelve el error original de la API
         } else {
           throw new Error('Error de red o de servidor'); // Manejar errores de red
         }
       }
     };
     
+    
     // Función para eliminar un usuario
-    export const deleteHotel = async (hotelId) => {
+
+    export const deleteHotel = async (id_hotel) => {
       try {
-        const response = await api.delete(`/hoteles/hoteles/delete/${hotelId}`, {
-          headers: {
-         'Content-Type': 'application/json'
-          }
-        });
+        const response = await api.delete(`/hoteles/hoteles/delete/${id_hotel}`);
         return response;
       } catch (error) {
         if (error.response) {
-          throw error; // Lanza el error para que lo maneje el store
+          throw error.response; // Devuelve el error original de la API
         } else {
           throw new Error('Error de red o de servidor'); // Manejar errores de red
         }
       }
     };
+    
     
