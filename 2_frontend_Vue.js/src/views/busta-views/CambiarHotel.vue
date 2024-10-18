9effe8ab9529bed1d2de51a207d0f81a8f88275c
Juan Bustamante <sebasbga7@gmail.com>
2024-10-17 19:35:36 -0500

    Alerta en cambio de hotel con validacion

--- 2_frontend_Vue.js/src/views/busta-views/CambiarHotel.vue
+++ 2_frontend_Vue.js/src/views/busta-views/CambiarHotel.vue
@@ -9,11 +9,16 @@ import FormControl from '@/components/FormControl.vue'
 import BaseButton from '@/components/BaseButton.vue'
 import LayoutGuest from '@/layouts/LayoutGuest.vue'
 import{ changeHotel } from '@/services/authService'
+import NotificationBar from '@/components/alejo_components/NotificationBar.vue';
 
 const email = ref('')
 const password = ref('')
 const errorMessage = ref(null)
 const newHotelCode = ref('')
+const isAlertVisible = ref(false);
+const colorAlert = ref('');
+const isModalVisible = ref(false);
+const modalMessage = ref('');
 
 const router = useRouter()
 
@@ -33,13 +38,25 @@ const changeHotelNew = async () => {
 
     try {
         await changeHotel(email.value, password.value, newHotelCode.value)
-        alert("¡Código de hotel actualizado con éxito!")
-        router.push("/") // Redirigir a la página de inicio después de cambiar el código de hotel
-    } catch (err) {
-        errorMessage.value = err.response?.data?.detail || "Error actualizando el código de hotel. Verifique el código."
+        
+        modalMessage.value = "¡Código de hotel actualizado con éxito! Regresa a login";
+        isAlertVisible.value = true;
+        colorAlert.value = 'success';
+
         setTimeout(() => {
-                errorMessage.value = null;
-            }, 3000);
+            isAlertVisible.value = false;
+        }, 10000);
+        
+    } catch (err) {
+
+            modalMessage.value = err.response?.data?.detail || "Error actualizando el código de hotel. Verifique el código.";
+            isAlertVisible.value = true;
+            colorAlert.value = 'danger';
+
+            setTimeout(() => {
+                isAlertVisible.value = false;
+            }, 10000);
+
     }
 }
 
@@ -49,6 +66,13 @@ const changeHotelNew = async () => {
     <LayoutGuest>
         <SectionFullScreen>
             <CardBox is-form @submit.prevent="changeHotelNew" class="sm:w-3/6 md:w-3/5 lg:w-3/6 xl:w-2/6 mx-auto">
+                <NotificationBar
+                    v-if="isAlertVisible"
+                    :color="colorAlert" 
+                    :description="modalMessage"
+                    :visible="isModalVisible"
+                />
+
                 <div class="flex justify-center mb-4">
                     <img src="@/assets/img/sena-agro.png" alt="Logo Sena" class="w-12 sm:w-16">
                 </div>
